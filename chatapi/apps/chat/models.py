47bbe6bbb4683121eec92e84c4fe06ed36b055c7
vgyn <maclin.kallat@novastonemedia.com>
2017-03-02 23:36:36 +0000

    Add Chat amd Message models

--- chatapi/apps/chat/models.py
+++ chatapi/apps/chat/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    title = models.TextField(max_length=100)
+    members = models.ManyToManyField(User)
+
+    class Meta:
+        default_related_name = 'chats'
+
+
+class Message(models.Model):
+    text = models.TextField(max_length=1000)
+    chat = models.ForeignKey(Chat)
